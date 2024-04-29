c06f878c39ed30c9c84df76567bac49809f46756
Manisha <manigetintouch@gmail.com>
2024-04-29 17:34:19 +0530

    Multi-item carousel created, css variables used, footer designed

--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
 
 const Layout = () => {
   return (
@@ -11,14 +12,7 @@ const Layout = () => {
       <main>
         <Outlet />
       </main>
-      <footer>
-        <ul>
-          <li>pages links</li>
-          <li>Social links</li>
-          <li>customer care support info</li>
-          <li>Since when this business is in</li>
-        </ul>
-      </footer>
+      <Footer />
     </>
   );
 };
